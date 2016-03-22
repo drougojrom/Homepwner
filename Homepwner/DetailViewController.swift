b3cd8929e4c72f4469fed12f0c3ed3042558cea5
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-22 12:05:05 +0300

    added working camera functionality and presentation on image view

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var serialNumberField: UITextField!
@@ -73,6 +73,34 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         view.endEditing(true)
     }
     @IBAction func takePicture(sender: UIBarButtonItem) {
+        
+        let imagePicker = UIImagePickerController()
+        // if your device has a camera, use it
+        // otherwise just pick an image from library
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            imagePicker.sourceType = .Camera
+        } else {
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        // place imagePicker on the screen
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // get picked image from into dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // put that image on the screen in the image view
+        imageView.image = image
+        
+        // take image picker off the screen -
+        // must call this dismiss method
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
 }
